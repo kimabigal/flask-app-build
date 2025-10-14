76df8f5671445e96c4c0619f608ec6a02a920726
kimabigal <abigaileleman@gmail.com>
2025-10-14 22:56:49 +0000

    corrected app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World New 2'
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
