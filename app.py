0a96dc4ee6807215dde49ab7cbd55fc4821074a3
kimabigal <abigaileleman@gmail.com>
2025-10-14 21:24:35 +0000

    fixed pw to docker's

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World New'
+    return 'Hello World New 2'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
